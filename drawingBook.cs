06540aac303e10d0ee6ccff67b9437a442203676
ljalmanzar <ljalmanzar@gmail.com>
2018-03-31 13:02:29 -0700

    drawing ook

---
+++ drawingBook.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static int solve(int n, int p)
+    {
+        int flips = 0;
+        // determine whether to start from back or end
+        if ((p-1)<(n-p)){
+            flips = (p - 1) / 2;
+            if ((p - 1) % 2 != 0)
+                flips++;
+        }
+        else{
+            flips = (n - p) / 2;
+            if ((n - p) % 2 != 0 && n % 2 == 0)
+                flips++;
+        }
+
+        return flips;
+    }
+
+    static void Main(String[] args)
+    {
+        int n = 18183;
+        int p = 18042;
+        int result = solve(n, p);
+        Console.WriteLine(result);
+    }
+}
