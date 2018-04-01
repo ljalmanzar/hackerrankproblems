3de217f440c1752414192fcda8789a24cbcf53e1
ljalmanzar <ljalmanzar@gmail.com>
2018-03-31 17:40:39 -0700

    counting valleys

---
+++ countingValleys.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static int countingValleys(int n, string s)
+    {
+        int depthTracker = 0;
+        int valleys = 0;
+
+        foreach (var ndx in s){
+            if (ndx == 'U'){
+                depthTracker++;
+            }
+            else {
+                depthTracker--;
+                if (depthTracker == -1)
+                    valleys++;
+            }
+        }
+
+        return valleys;
+    }
+
+    static void Main(String[] args)
+    {
+        int n = 8;
+        string s = "UDDDUDUU";
+        int result = countingValleys(n, s);
+        Console.WriteLine(result);
+    }
+}
